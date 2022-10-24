9cca2511f797a1ecee43de5eb2beeffb37330642
Joao Danilo <joaodanilo1992@gmail.com>
2022-10-24 07:07:37 -0400

    Fixing Keystore2 crashes

--- meuproduto.mk
+++ meuproduto.mk
@@ -83,3 +83,9 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service.software
 # ================= END =================
+
+# ================= keystore2 =================
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+# ================= END =================
